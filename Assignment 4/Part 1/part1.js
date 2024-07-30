0048135ef8205a31fe69e9841042e882ee535bc3
Nirmal6120 <nirmalmilindkumar.patel@dcmail.ca>
2024-07-29 20:44:30 -0400

    assignment 4 part 1 7th commit
    making changes in java script raw file

--- Assignment 4/Part 1/part1.js
+++ Assignment 4/Part 1/part1.js
@@ -3,7 +3,7 @@
      Discription: fourth HTML assignment part 1
      it is the actual java script code */
 
-1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+// COMPLETE VARIABLE AND FUNCTION DEFINITIONS
 
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
@@ -14,23 +14,16 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-2. RAW TEXT STRINGS
+// RAW TEXT STRINGS
 
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-Willy the Goblin
-Big Daddy
-Father Christmas
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
-the soup kitchen
-Disneyland
-the White House
 
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
-
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+// EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
