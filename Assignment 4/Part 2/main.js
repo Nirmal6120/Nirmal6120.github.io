52a45b9126ddc02d68ca2b78ee397e34d4ae38d9
Nirmal6120 <nirmalmilindkumar.patel@dcmail.ca>
2024-07-30 00:42:33 -0400

    assignment 4 part 2 3rd commit
    commit after making adding arrays for the images and adding array for the alt text for the images. and also added the for loop to loop through images.

--- Assignment 4/Part 2/main.js
+++ Assignment 4/Part 2/main.js
@@ -6,15 +6,23 @@
 
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFiles = ['pic1.jpg' , 'pic2.jpg', 'pic3.jpg' ,'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altText = ['picture of eye', 'painting of the desert', 'painting of the flowers'
+    , 'painting of the ancient peoples' , 'butterfly on leaf']
 
 /* Looping through images */
+for(let i=0; i<imageFiles.length; i++){
+    const Images = document.createElement('img');
+    Images.setAttribute('src',`images/${imageFiles[i]}`);
+    Images.setAttribute('alt', altText[i]);
+    thumbBar.appendChild(newImage);
+}
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
