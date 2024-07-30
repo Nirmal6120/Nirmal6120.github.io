08913a48af4a3b8c07d1fbe1399b6dc3a9b66827
Nirmal6120 <nirmalmilindkumar.patel@dcmail.ca>
2024-07-30 03:19:40 -0400

    assignment 4 part 2 10 th commit
    adding comments in the java script code

--- Assignment 4/Part 2/main.js
+++ Assignment 4/Part 2/main.js
@@ -3,10 +3,13 @@
      Discription: fourth HTML assignment part 2
      it is the actual java script code */
 
-
+// it is used to display the main image element.
 const displayedImage = document.querySelector('.displayed-img');
+// it is used to select container for thumb nail.
 const thumbBar = document.querySelector('.thumb-bar');
+// used for the button
 const btn = document.querySelector('button');
+// use to overlay the images
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
@@ -22,14 +25,23 @@ const altText = [
 ];
 
 /* Looping through images */
+// uses for loop for looping the images.
 for(let i = 0; i < imageFiles.length; i++) {
+  // creates new image element every time the loop occurs. 
   const newImage = document.createElement('img');
+  // link for the source of the images.  
   newImage.setAttribute('src',`images/${imageFiles[i]}`);
+  // uses link of array for alttext   
   newImage.setAttribute('alt', altText[i]);
+  // adding new image ti thumbBar.   
   thumbBar.appendChild(newImage);
 
+  // eventlistener for clicking on the image
+  // e stands for event objects.  
   newImage.addEventListener('click',(e) => {
+    // set source of display image to target source.
     displayedImage.src = e.target.src;
+    // set source of display image alt to target source alt.
     displayedImage.alt = e.target.alt;
   });
 }
@@ -37,13 +49,17 @@ for(let i = 0; i < imageFiles.length; i++) {
 
 /* Wiring up the Darken/Lighten button */
 
+// eventlistner for the light effect
 btn.addEventListener('click', ()=> {
+  // helps to get the current class of button.
   const btnClass = btn.getAttribute('class');
+  // if button is dark it will change to light.
   if(btnClass === 'dark'){
     btn.setAttribute('class','light'); 
     btn.textContent = 'Lighten';
     overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
   } else {
+       // if button is light it will change to dark. 
        btn.setAttribute('class','dark');
        btn.textContent = 'Darken';
        overlay.style.backgroundColor = 'rgba(0,0,0,0)'; 
