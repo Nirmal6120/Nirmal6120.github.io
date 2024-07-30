e4746807ce1d5c988e56291cc98a1264196579b9
Nirmal6120 <nirmalmilindkumar.patel@dcmail.ca>
2024-07-29 21:10:26 -0400

    assignment 4 part 1 9th commit
    correct the error i have written insertx in each and every new story replace syntax which i change to x y and z to enter the values which is in x y and z array

--- Assignment 4/Part 1/part1.js
+++ Assignment 4/Part 1/part1.js
@@ -16,7 +16,7 @@ function randomValueFromArray(array){
 
 // RAW TEXT STRINGS
 
-const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — weighs 300 pounds, and it was a hot day.";
 
 const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
@@ -35,8 +35,8 @@ function result() {
     const zItem = randomValueFromArray(insertZ);
 
     newStory = newStory.replace(":insertx:",xItem);
-    newStory = newStory.replace(":insertx:",yItem);
-    newStory = newStory.replace(":insertx:",zItem);
+    newStory = newStory.replace(":inserty:",yItem);
+    newStory = newStory.replace(":insertz:",zItem);
 
 
 
@@ -46,8 +46,8 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300/14) + 'stone';
-    const temperature =  Math.round((94 - 32) * 5 /9) + 'centigrade';
+    const weight = Math.round(300/14) + ' stone';
+    const temperature =  Math.round((94 - 32) * 5 /9) + ' centigrade';
     newStory = newStory.replace('300 pounds', weight);
     newStory = newStory.replace('94 fahrenheit', temperature);
 
