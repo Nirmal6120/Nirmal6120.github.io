c5f5d06cb8c177a25bb8b47c361700ba64ef6b74
Nirmal6120 <nirmalmilindkumar.patel@dcmail.ca>
2024-07-31 01:19:26 -0400

    assignment 4 part 3 9th commit
    adding collison method to the ball class so the color of the ball changes when the collied

--- Assignment 4/Part 3/main.js
+++ Assignment 4/Part 3/main.js
@@ -56,4 +56,18 @@ class Ball {
         this.x += this.veIX;
         this.y += this.veIY;
     }
+    collisionDetect() {
+        for (const ball of balls) {
+          if (this !== ball) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+      
+            if (distance < this.size + ball.size) {
+              ball.color = this.color = randomRGB();
+            }
+          }
+        }
+      }
+
 }
