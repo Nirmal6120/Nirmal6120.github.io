49ed339585956994613295f44e5ff35c0b16a8f7
Nirmal6120 <nirmalmilindkumar.patel@dcmail.ca>
2024-08-07 17:20:12 -0400

    assignment 5 final commit
    making some changes in html file and adding comments in all the 3 files

--- Assignment 5/assessment-files/assessment-files/main.js
+++ Assignment 5/assessment-files/assessment-files/main.js
@@ -8,13 +8,19 @@
 
 // functionality for showing/hiding the comments section
 
+// it will select show-hide button for Document Object Model.
 const showHideBtn = document.querySelector('.show-hide');
+// it will select comment wrapper element 
 const commentWrapper = document.querySelector('.comment-wrapper');
 
+// initialise hide comment wrapper.
 commentWrapper.style.display = 'none';
 
+// it will add event listener.
 showHideBtn.onclick = function() {
+  // it will get the current text content of button
   let showHideText = showHideBtn.textContent;
+  // checks the condition if the text is show comment
   if(showHideText === 'Show comments') {
     showHideBtn.textContent = 'Hide comments';
     commentWrapper.style.display = 'block';
@@ -26,16 +32,20 @@ showHideBtn.onclick = function() {
 
 // functionality for adding a new comment via the comments form
 
+// get the comments form element
 const form = document.querySelector('.comment-form');
+// select the name from DOM
 const nameField = document.querySelector('#name');
 const commentField = document.querySelector('#comment');
 const list = document.querySelector('.comment-container');
 
+// add event listener to submit the form 
 form.onsubmit = function(e) {
   e.preventDefault();
   submitComment();
 };
 
+// function to submit new comments.
 function submitComment() {
   const listItem = document.createElement('li');
   const namePara = document.createElement('p');
@@ -50,6 +60,8 @@ function submitComment() {
   listItem.appendChild(namePara);
   listItem.appendChild(commentPara);
 
+  // it will clear name input field
   nameField.value = '';
+  // clears comment input 
   commentField.value = '';
 }
